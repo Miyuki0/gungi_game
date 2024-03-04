6954aa55c92b44afb85dd2c9e2cb72229101cc28
Jérémy Roussey <jeremyroussey@yahoo.fr>
2024-03-04 12:18:53 +0100

    Fixed tier1 moves + added test func

--- main.ts
+++ main.ts
@@ -1,15 +1,36 @@
 class Piece{
-    //to-do add tiers
-    constructor(public color: string, public type: string){}
+    constructor(public color: string, public type: string, public canBeStacked: boolean, public isStacked: boolean, public subPieces: Piece[]){}
 
-    isValidMove(start: [number, number], end: [number, number]){
+    isValidMove(start: [number, number], end: [number, number]): boolean{
         throw new Error("Method 'isValidMove' must be implemented.");
+    }
+
+    getStacked(){
+        this.isStacked = true;
+    }
+
+    getTier(){
+        return this.subPieces.length + 1;
+    }
+
+    canStackOnTopOf(Piece piece){
+        return piece.canBeStacked;
+    }
+
+    stackOnTopOf(Piece piece){
+        piece.getStacked()
+        this.subPieces.push(piece);
+        this.subPieces.push(piece.subPieces)
+
+        if (this.getTier() == MAX_TIER){
+            this.canBeStacked = false
+        }
     }
 }
 
 class Marshall extends Piece{
     constructor(color: string){
-        super(color, "Marshall");
+        super(color, "Marshall", false, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -24,33 +45,33 @@ class Marshall extends Piece{
 
 class Pawn extends Piece{
     constructor(color: string){
-        super(color, "Pawn");
+        super(color, "Pawn", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
         const [startRow, startCol] = start;
         const [endRow, endCol] = end;
 
-        return startCol == endCol && (endRow - startRow) == 0;
+        return startCol == endCol && (startRow - endRow) == 1;
     }
 }
 
 class Spy extends Piece{
     constructor(color: string){
-        super(color, "Spy");
+        super(color, "Spy", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
         const [startRow, startCol] = start;
         const [endRow, endCol] = end;
 
-        return startCol == endCol && (endRow - startRow) == 0;
+        return startCol == endCol && (startRow - endRow) == 1;
     }
 }
 
 class Cannon extends Piece{
     constructor(color: string){
-        super(color, "Cannon");
+        super(color, "Cannon", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -69,7 +90,11 @@ class Cannon extends Piece{
 
 class Fortress extends Piece{
     constructor(color: string){
-        super(color, "Fortress");
+        super(color, "Fortress", false, false, []);
+    }
+
+    canStackOnTopOf(Piece piece){
+        return false;
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -84,7 +109,7 @@ class Fortress extends Piece{
 
 class Samurai extends Piece{
     constructor(color: string){
-        super(color, "Samurai");
+        super(color, "Samurai", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -99,7 +124,7 @@ class Samurai extends Piece{
 
 class Captain extends Piece{
     constructor(color: string){
-        super(color, "Captain");
+        super(color, "Captain", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -114,20 +139,20 @@ class Captain extends Piece{
 
 class Musketeer extends Piece{
     constructor(color: string){
-        super(color, "Musketeer");
+        super(color, "Musketeer", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
         const [startRow, startCol] = start;
         const [endRow, endCol] = end;
 
-        return startCol == endCol && (endRow - startRow) == 0;
+        return startCol == endCol && (startRow - endRow) == 1;
     }
 }
 
 class Knight extends Piece{
     constructor(color: string){
-        super(color, "Knight");
+        super(color, "Knight", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -137,15 +162,15 @@ class Knight extends Piece{
         const colDifference = Math.abs(startCol - endCol);
 
         return (
-            (startCol == endCol && rowDifference == 1) ||
-            (rowDifference == 1 && (endCol - startCol == 2))
+            (startRow == endRow && colDifference == 1) ||
+            (startRow - endRow == 2 && colDifference == 1)
             )
     }
 }
 
 class Archer extends Piece{
     constructor(color: string){
-        super(color, "Archer");
+        super(color, "Archer", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -160,7 +185,7 @@ class Archer extends Piece{
 
 class General extends Piece{
     constructor(color: string){
-        super(color, "General");
+        super(color, "General", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -171,14 +196,14 @@ class General extends Piece{
 
         return (
             (rowDifference <= 1 && colDifference <= 1) &&
-            !(startRow - endRow == 1 && colDifference == 1)
+            !(endRow - startRow == 1 && colDifference == 1)
         )
     }
 }
 
 class LieutenantGeneral extends Piece{
     constructor(color: string){
-        super(color, "Lieutenant General");
+        super(color, "Lieutenant General", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -189,14 +214,14 @@ class LieutenantGeneral extends Piece{
 
         return (
             (rowDifference == 1 && colDifference <= 1) &&
-            !(startRow - endRow == 1 && colDifference == 1)
+            !(endRow - startRow == 1 && colDifference != 1)
         );
     }
 }
 
 class MajorGeneral extends Piece{
     constructor(color: string){
-        super(color, "Major General");
+        super(color, "Major General", true, false, []);
     }
 
     isValidMove(start: [number, number], end: [number, number]): boolean{
@@ -206,10 +231,44 @@ class MajorGeneral extends Piece{
         const colDifference = Math.abs(startCol - endCol);
 
         return (
-            (endRow - startRow == 0 && colDifference <= 1) &&
-            (endRow != endCol)
+            (colDifference == 1 && startRow - endRow == 1)
         );
     }
 }
 
 const MAX_PIECES_BY_PLAYER = 26;
+const MAX_TIER = 3;
+
+class Board {
+    private board: (Piece | null)[][];
+
+    constructor() {
+        this.board = Array(8).fill(null).map(() => Array(8).fill(null));
+    }
+}
+
+class Main {
+    static main() {
+        function generateMoveGrid(piece: any): string[][] {
+            const gridSize = 5;
+            const grid: string[][] = Array(gridSize).fill(null).map(() => Array(gridSize).fill("O"));
+
+            for (let row = 0; row < gridSize; row++) {
+                for (let col = 0; col < gridSize; col++) {
+                    if (piece.isValidMove([2, 2], [row, col])) {
+                        grid[row][col] = "X";
+                    }
+                }
+            }
+            grid[2][2] = "M"
+
+            return grid;
+        }
+
+        console.log(generateMoveGrid(new MajorGeneral("")));
+
+    }
+}
+
+// Call the main function
+Main.main();
